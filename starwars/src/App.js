50d10e2a8c95174e86e4af59ce99b69d353decbf
Brett Karr <b.karr@polyclip.us>
2019-09-28 13:03:32 -0500

    commit with MVP met

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
+// import axios from "axios";
 //import components
-//import /name/ from "./components/{Name}.jsx"\
 import CharacterList from "./components/CharacterList.jsx";
 
 const App = () => {
@@ -12,20 +11,6 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
-  const [siteContent, setContent] = useState([]);
-
-  // useEffect(() => {
-  // axios
-  //   // .get(`https://swapi.co/api/people/${1}`)
-  //   .get(`https://swapi.co/api/people/`)
-  //   .then(results => {
-  //     console.log(Object.keys(results.data));
-  //     console.log(results.data[`results`]);
-  //     console.log(results.data);
-  //   })
-  //   .catch(err => console.log(err));
-  // }, []);
 
   return (
     <div className="App">
--- starwars/src/components/CharacterCard.jsx
+++ starwars/src/components/CharacterCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { Card, CardText, CardBody, CardTitle, CardImg, Col } from "reactstrap";
 
 const CharacterCard = props => {
   return (
-    <div className="char-card">
-      <h2>Character name: {props.name}</h2>
-      <div>
-        <p>Character Descriptions:</p>
-        <p>Height: {props.height}</p>
-        <p>Homeworld: {props.homeworld}</p>
-        <p>Hair Color: {props.hair_color}</p>
-      </div>
-    </div>
+    <Col xs="6" lg="4" height="100%">
+      <Card height="100%">
+        <CardImg src="" width="100%" top />
+        <CardBody height="100%">
+          <CardTitle>Character name: {props.name}</CardTitle>
+          <hr />
+          <CardText>Character Descriptions:</CardText>
+          <CardText>Height: {props.height}</CardText>
+          <CardText>Homeworld: {props.homeworld}</CardText>
+          <CardText>Hair Color: {props.hair_color}</CardText>
+          {/* <CardText height="100%">Other details...</CardText> */}
+        </CardBody>
+      </Card>
+    </Col>
   );
 };
 
