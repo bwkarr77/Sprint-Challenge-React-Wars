179f5a020f13c83d68b88d71b26a16d254e72ea1
Brett Karr <b.karr@polyclip.us>
2019-09-28 14:00:27 -0500

    commit with MVP and answers

--- starwars/src/components/CharacterCard.jsx
+++ starwars/src/components/CharacterCard.jsx
@@ -3,11 +3,11 @@ import { Card, CardText, CardBody, CardTitle, CardImg, Col } from "reactstrap";
 
 const CharacterCard = props => {
   return (
-    <Col xs="6" lg="4" height="100%">
-      <Card height="100%">
+    <Col xs="6" lg="4" height="100%" background="blue" color="yellow">
+      <Card height="5000px">
         <CardImg src="" width="100%" top />
         <CardBody height="100%">
-          <CardTitle>Character name: {props.name}</CardTitle>
+          <CardTitle font-size="50px">Character name: {props.name}</CardTitle>
           <hr />
           <CardText>Character Descriptions:</CardText>
           <CardText>Height: {props.height}</CardText>
